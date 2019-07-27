ba63ec8a387f193338aad6e269f5e65d563d78e4
abhirup-deb <debabhirup1999@gmail.com>
2019-07-27 12:23:25 +0530

    Dynamic Slider  Added

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -13,6 +13,7 @@ class _InputPageState extends State<InputPage> {
   Color mclr = Color(0xFF111328);
   Color active = Color(0xFF1D1E33);
   Color inactive = Color(0xFF111328);
+  int height = 160;
 
   void Gesture(int gdr){
     if(gdr==1) {
@@ -36,58 +37,70 @@ class _InputPageState extends State<InputPage> {
     return Scaffold(
       appBar: AppBar(
         title: new Center(child: Text('BMI CALCULATOR'),),
-        
+
       ),
       body: Column(
         children: <Widget>[
           Row(
-              children: <Widget>[
-                Expanded(
-                  child: GestureDetector(
-                    onTap: (){
-                      setState(() {
-                        Gesture(1);
-                      });
-                    },
-                    child: ReusableCard(clr: mclr,
+            children: <Widget>[
+              Expanded(
+                child: GestureDetector(
+                  onTap: (){
+                    setState(() {
+                      Gesture(1);
+                    });
+                  },
+                  child: ReusableCard(clr: mclr,
                     cardChild:  IconContent(gender: 'MALE',sign: FontAwesomeIcons.mars,),
-                    ),
                   ),
                 ),
-                Expanded(
-                  child: GestureDetector(
+              ),
+              Expanded(
+                child: GestureDetector(
                     onTap: (){
                       setState(() {
                         Gesture(2);
                       });
                     },
-                      child: ReusableCard(
-                        clr: fclr,
-                        cardChild: IconContent(gender: 'FEMALE',sign: FontAwesomeIcons.venus),
-                      )
-                  ),
+                    child: ReusableCard(
+                      clr: fclr,
+                      cardChild: IconContent(gender: 'FEMALE',sign: FontAwesomeIcons.venus),
+                    )
                 ),
-              ],
-            ),
+              ),
+            ],
+          ),
           Expanded(
             child:ReusableCard(clr: Color(0xFF1D1E33),
-            cardChild: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Text('HEIGHT'),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.baseline,
-                  textBaseline: TextBaseline.alphabetic,
-                  children: <Widget>[
-                    Text(height.toString(),style: TextStyle(
-                      fontSize: 38.0,
-                      fontWeight: FontWeight.w900,
-                    ),),
-                    Text('cm'),
-                  ],),
-              ],
-            ),
+              cardChild: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text('HEIGHT'),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.baseline,
+                    textBaseline: TextBaseline.alphabetic,
+                    children: <Widget>[
+                      Text(height.toString(),style: TextStyle(
+                        fontSize: 38.0,
+                        fontWeight: FontWeight.w900,
+                      ),),
+                      Text('cm'),
+                    ],),
+                  Slider(
+                    value: height.toDouble(),
+                    min: 120,
+                    max: 220,
+                    activeColor: Color(0xFFEB1555),
+                    inactiveColor: Color(0xFF8D8E98),
+                    onChanged: (double newValue){
+                      setState(() {
+                        height= newValue.round();
+                      });
+                    },
+                  ),
+                ],
+              ),
             ),
           ),
           Row(
@@ -109,9 +122,9 @@ class _InputPageState extends State<InputPage> {
 }
 
 class IconContent extends StatelessWidget {
- IconContent({@required this.gender,@required this.sign});
- final String gender;
- final IconData sign;
+  IconContent({@required this.gender,@required this.sign});
+  final String gender;
+  final IconData sign;
 
   @override
   Widget build(BuildContext context) {
@@ -119,7 +132,7 @@ class IconContent extends StatelessWidget {
       mainAxisAlignment: MainAxisAlignment.center,
       children: <Widget>[
         Icon(
-           sign,
+          sign,
           size: 60.0,
           color: Colors.white,
         ),
@@ -127,10 +140,10 @@ class IconContent extends StatelessWidget {
           height: 6.0,
         ),
         Text(gender,
-        style: TextStyle(
-          color: Color(0xFF70FFC0),
-          height: 1.0,
-        )),
+            style: TextStyle(
+              color: Color(0xFF70FFC0),
+              height: 1.0,
+            )),
       ],
     );
   }
@@ -138,6 +151,7 @@ class IconContent extends StatelessWidget {
 
 class ReusableCard extends StatelessWidget {
   ReusableCard({@required this.clr, this.cardChild});
+
   final Color clr;
   final Widget cardChild;
 
