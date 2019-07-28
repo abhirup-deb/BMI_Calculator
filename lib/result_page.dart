ba1899c840b6794dd24161f1f8aaf2347401118c
abhirup-deb <debabhirup1999@gmail.com>
2019-07-28 13:22:25 +0530

    2nd Page UI Added

--- lib/result_page.dart
+++ lib/result_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'input_page.dart';
+
 
 class Resultpage extends StatelessWidget {
   @override
@@ -6,7 +8,45 @@ class Resultpage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: new Center(child: Text('BMI CALCULATOR'),),
+      ),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: Text('Your Result',style: TextStyle(
+              fontSize: 38.0,
+              fontWeight: FontWeight.w700,
+            ),),
+          ),
+          Expanded(
+            flex: 5,
+            child: ReusableCard(clr: Color(0xFF1D1E33),
+                cardChild: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: <Widget>[
+                    Center(
+                      child:  Text('Normal',style: TextStyle(color: Colors.white,fontSize: 18.0),),
+                    ),
+                    Center(child: Text('29.6',style: TextStyle(color: Colors.white,fontWeight: FontWeight.w900,fontSize: 85.0),)),
+                    Center(child: Text('Someting important')),
+                  ],
+                )
+            ),
+          ),
 
+          GestureDetector(
+            onTap: () {
+              Navigator.push(context, MaterialPageRoute(builder: (context) => InputPage()),);
+            },
+            child: Container(
+              color: Color(0xFFEB1555),
+              margin: EdgeInsets.only(top: 1.0),
+              width: double.infinity,
+              height: 60.0,
+              child: Center(child: Text('RE-CALCULATE',style: TextStyle(fontSize: 28.0),),),
+            ),
+          ),
+        ],
       ),
     );
   }
